8719cde14786ed934c72925767108356d58a97df
Ariel Carvalho Nascimento <arielcarvnasc@gmail.com>
2023-01-10 00:21:47 -0300

    feat: First commit TicTacToe

---
+++ TicTacToe/src/App.tsx
@@ -0,0 +1,21 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import './App.css'
+
+function App() {
+  const getSquares = () => {
+    return new Array(9).fill(true);
+  }
+
+  return (
+    <div className="container">
+      <div className="board">
+        {getSquares().map((_, i) => (
+          <div className='cell'>X</div>
+          ))}
+        </div>
+    </div>
+  )
+}
+
+export default App
